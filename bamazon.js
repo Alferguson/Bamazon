2b3bff020371c5c18d6dbe6b37608acffcd82fb8
Alferguson <alferguson@berkeley.edu>
2017-11-13 07:57:35 -0800

    first part of hw

--- bamazon.js
+++ bamazon.js
@@ -1,70 +1,115 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table2');
 
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+// creates table
+var table = new Table({
+    head: ["Item ID", "Product", "Department", "Price", "Stock Quantity"],
+    colWidths: [4, 35, 15, 6, 20],
+    chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+           , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+           , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+           , 'right': '║' , 'right-mid': '╢' , 'middle': '│' }    
+});
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "root",
-  database: "bamazon_DB"
+  database: "bamazon_db"
 });
 
-connection.connect(function(error) {
-  if (error) throw error;
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected");
   startUp();
+  inquire();
+
 });
 
 // displays table of data
 var startUp = function() {
-	var query = "SELECT * FROM bamazon_DB";
-  	connection.query(query, function(error, res) {
-    	for (var i = 0; i < res.length; i++) {
-      		console.log (
-        		"Item ID: " +
-          		res[i].item_id +
-          		" || Product: " +
-          		res[i].product_name +
-          		" || Department: " +
-          		res[i].department_name +
-          		" || Price: " +
-          		res[i].price +          		
-          		" || Stock Quantity: " +
-          		res[i].stock_quantity
-      		);
-    	}
-    connection.end();
-    }	
+	var query = "SELECT * FROM products";
+	connection.query(query, function(err, res) {
+  	for (var i = 0; i < res.length; i++) {
+      table.push(
+        [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+      );      
+  	}
+    // creates tables from res values
+    console.log(table.toString());
+  }) 	
+
 }
 
-// inquirer
 var inquire = function() {
-	inquirer
-	    .prompt(
-	    {
-	      	name: "productID",
-	      	type: "input",
-	     	message: "What product ID would you like to choose?"
-	    }
-	    {
-	    	name: "units",
-	      	type: "input",
-	     	message: "How much of this item would you like to purchase?"
-	    }
-	    )
-	    .then(function(answer) {
-	      // based on their answer, either call the bid or the post functions
-	      if (answer.postOrBid.toUpperCase() === "POST") {
-	        postAuction();
-	      }
-	      else {
-	        bidAuction();
-	      }
-	    });
-}    
+	inquirer.prompt([
+    {
+      name: "productID",
+      type: "input",
+     	message: "What product ID would you like to choose?"
+    },
+    {
+    	name: "units",
+      type: "input",
+     	message: "How much of this item would you like to purchase?"
+    }
+  ])
+  .then(function(answer) {
+
+    connection.query("SELECT item_id, product_name, stock_quantity FROM products", function(err, res) {
+      for (var j = 0; j < res.length; j++) {
+        if (res[j].item_id === parseInt(answer.productID)) {
+          if (res[j].stock_quantity >= parseInt(answer.units)) {
+            productName = res[j].product_name;
+            quanDif = res[j].stock_quantity - parseInt(answer.units);
+            id = res[j].item_id;
+            connection.query("UPDATE products SET ? WHERE ?",
+              [{
+                stock_quantity: quanDif
+              },
+              {
+                item_id: id
+              }],
+              function(err, result) {
+                if (err) throw error;
+                console.log("Your " + productName + " will arrive in 7 days, thank you for your purchase!\n");
+              }
+            );
+          }
+          else {
+            console.log("Sorry, not enough of that item exists");
+          }
+        }
+      }  
+    connection.end();
+    });  
+  })  
+}   
+
+// var update = function() {
+//   connection.query("SELECT item_id, product_name, stock_quantity FROM products", function(err, res) {
+//     for (var j = 0; j < res.length; j++) {
+//       if (res[j].item_id === parseInt(answer.productID)) {
+//         if (res[j].stock_quantity >= parseInt(answer.units)) {
+//           console.log(res[j].product_name);
+//           var query = connection.query("UPDATE products SET ? WHERE ?",
+//             {
+//               stock_quantity: res[j].stock_quantity - parseInt(answer.units)
+//             },
+//             function(err, res) {
+//               // console.log(res.changedRows);
+//               // console.log("Your " + res[j].product_name + " will arrive in 7 days, thank you for your purchase!\n");
+//             }
+//           );
+//         }
+//         else {
+//           console.log("Sorry, not enough of that item exists");
+//         }
+//       }
+//     }  
+//   console.log("eh");
+//   });
+// connection.end();      
+// }
--- bamazon.sql
+++ bamazon.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS bamazon_DB;
 CREATE DATABASE bamazon_DB;
 
-USE greatBay_DB;
+USE bamazon_DB;
 
 CREATE TABLE products(
   item_id INTEGER(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -11,5 +11,15 @@ CREATE TABLE products(
   stock_quantity INT NOT NULL
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity);
-VALUES ("Somewhat warm Pizza", "Food-Depo", 20, 500), ("Dull Hair Trimmers", "Health-Depo", 2, 500000000), ("A pickle that can be used as a tv remote", "TV-Depo", 4, 3200);
+INSERT INTO products 
+	(product_name, department_name, price, stock_quantity)
+VALUES ("Somewhat Warm Pizza", "Food-Depo", 20, 500), 
+	("Dull Hair Trimmers", "Health-Depo", 2, 500000000), 
+	("A pickle that can be used as a tv remote", "TV-Depo", 4, 3200), 
+	("Gnarly pickes", "Food-Depo", 2, 100), 
+	("An in-depth analysis of the Bee Movie", "DVD-Depo", 5000, 4), 
+	("A cake with salami in it", "Food-Depo", 60, 75), 
+	("Knack", "VideoGame-Depo", 100, 9500), 
+	("A pair of shoes but both are lefties", "Clothes-Depo", 6, 17), 
+	("Invisible lizards", "Pet-Depo", 40, 2000500), 
+	("lil wayne's YEEEAAAHHHHH", "???-Depo", 64, 1);
